3b5db736f3cbc69b32a3c48adc1ad8c8a3c1b371
Masanari Mizuno <mznms@primu.la>
2024-01-29 04:37:42 +0900

    OGP画像用のフォントを指定

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Codes are licensed under the [MIT License](https://opensource.org/license/mit/).
 Blog contents are licensed under [the Creative Commons Attribution-ShareAlike 4.0 International License](https://creativecommons.org/licenses/by-sa/4.0/).
 
 This blog is based on the Astro theme, "[AstroPaper](https://github.com/satnaing/astro-paper)" made by [Sat Naing](https://satnaing.dev) and [contributors](https://github.com/satnaing/astro-paper/graphs/contributors).
+
+This site uses [Line Seed JP](https://seed.line.me/index_jp.html) font for generating Open Graph Images.
--- src/utils/generateOgImages.tsx
+++ src/utils/generateOgImages.tsx
@@ -7,13 +7,13 @@ import siteOgImage from "./og-templates/site";
 const fetchFonts = async () => {
   // Regular Font
   const fontFileRegular = await fetch(
-    "https://www.1001fonts.com/download/font/ibm-plex-mono.regular.ttf"
+    "https://img.mamizu.jp/LINESeedJP_A_TTF_Rg.ttf"
   );
   const fontRegular: ArrayBuffer = await fontFileRegular.arrayBuffer();
 
   // Bold Font
   const fontFileBold = await fetch(
-    "https://www.1001fonts.com/download/font/ibm-plex-mono.bold.ttf"
+    "https://img.mamizu.jp/LINESeedJP_A_TTF_Bd.ttf"
   );
   const fontBold: ArrayBuffer = await fontFileBold.arrayBuffer();
 
@@ -28,13 +28,13 @@ const options: SatoriOptions = {
   embedFont: true,
   fonts: [
     {
-      name: "IBM Plex Mono",
+      name: "LINE Seed JP",
       data: fontRegular,
       weight: 400,
       style: "normal",
     },
     {
-      name: "IBM Plex Mono",
+      name: "LINE Seed JP",
       data: fontBold,
       weight: 600,
       style: "normal",
